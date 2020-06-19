754a8166355fbc249d5def41883cac59edb922b5
Nicolas <nicolas@racchi.com>
2020-06-19 21:48:45 +0200

    CLI completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Stargazer
+# Stargazer
+
+> A simple CLI tool to monitor the **stars**, **watchers**, and **forks** of any repository.
--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const { askRepo } = require("./lib/inquirer");
+const { askRepo, askFrequency } = require("./lib/inquirer");
 const { init, reset, printStats } = require("./lib/tools");
 const { watchStars } = require("./lib/watcher");
 
 const run = async () => {
   init();
   const repo = await askRepo();
+  const frequency = await askFrequency();
   update(repo);
 
   setInterval(async function () {
     update(repo);
-  }, 5000);
+  }, frequency * 1000);
 };
 
 const update = async (repo) => {
   reset(repo);
   const info = await watchStars(repo);
-  printStats(info);
-  console.log("\n", "Press option+C to exit");
+  if (info !== null) {
+    printStats(info);
+    console.log("\n", "Press option+C to exit");
+  } else {
+    process.kill(process.pid);
+  }
 };
 
 run();
