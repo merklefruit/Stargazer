6331aa57c96be2f559a78d6cdd28beb6cf73710c
Nicolas <nicolas@racchi.com>
2020-06-20 10:23:01 +0200

    Added github auth

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-const { askRepo, askFrequency } = require("./lib/inquirer");
-const { init, reset, printStats } = require("./lib/tools");
+const { askRepo, askToken } = require("./lib/inquirer");
+const {
+  init,
+  reset,
+  printStats,
+  saveToken,
+  getToken,
+  explainAuth,
+} = require("./lib/tools");
 const { watchStars } = require("./lib/watcher");
 
 const run = async () => {
   init();
+
+  await checkAuth();
   const repo = await askRepo();
-  const frequency = await askFrequency();
   update(repo);
 
   setInterval(async function () {
     update(repo);
-  }, frequency * 1000);
+  }, 10000);
 };
 
 const update = async (repo) => {
   reset(repo);
   const info = await watchStars(repo);
-  if (info !== null) {
-    printStats(info);
-    console.log("\n", "Press option+C to exit");
-  } else {
+  if (info === null) {
     process.kill(process.pid);
+  }
+  printStats(info);
+  console.log("\n", "Press option+C to exit");
+};
+
+const checkAuth = async () => {
+  const token = getToken().token;
+  if (token) {
+    console.log("Successfully authenticated!", "\n");
+  } else {
+    explainAuth();
+    const newToken = await askToken();
+    saveToken(newToken);
+    return newToken;
   }
 };
 
